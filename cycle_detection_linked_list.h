8ca8221e4a383213f88ced330d92f1168ab1171f
Rupesh Kumar <65998605+codechamp379@users.noreply.github.com>
2023-07-01 23:59:36 +0530

    Add files via upload

---
+++ cycle_detection_linked_list.h
@@ -0,0 +1,45 @@
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+
+
+*****************************************************************/
+
+bool detectCycle(Node *head)
+{
+	//	Write your code here
+    //logic: In a circle, if a pointer moves with x speed and other pointer moves
+    //with 2x speed then they gonna defintely meet at a point
+    Node *slow = head;
+    Node *fast = head;
+    while(fast!=NULL && fast->next!=NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow==fast) //we got the loop
+          return true;
+    }
+    return false;
+}
